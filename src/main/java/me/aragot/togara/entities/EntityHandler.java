0cdcee5503510aeb09722e95e0f378cdd41e819d
togarAragot <the.aragot@gmail.com>
2023-05-15 18:39:23 +0200

    Added Knockback calculations and registering TogaraEntities on world join as well as custom entities base instances

--- src/main/java/me/aragot/togara/entities/EntityHandler.java
+++ src/main/java/me/aragot/togara/entities/EntityHandler.java
@@ -1,25 +1,29 @@
 package me.aragot.togara.entities;
 
 import me.aragot.togara.Togara;
+import me.aragot.togara.entities.enemies.TestMob;
 import me.aragot.togara.entities.player.TogaraPlayer;
+import org.bukkit.NamespacedKey;
 import org.bukkit.World;
-import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.*;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.util.Vector;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class EntityHandler {
 
+    private ArrayList<TogaraEntity> customEntities = new ArrayList<>();
     private ArrayList<TogaraEntity> entityList = new ArrayList<>();
     private ArrayList<TogaraEntity> toRemove = new ArrayList<>();
+    private ArrayList<World> loadedWorlds = new ArrayList<>();
     public HashMap<TogaraEntity, DamageType> damageQueue = new HashMap<>();
 
 
     public EntityHandler(){
-        register();
+        initCustomEntities();
     }
 
     public void attackEntity(Player player, Mob entity){
@@ -27,6 +31,15 @@ public class EntityHandler {
         TogaraEntity tEntity = getTogaraEntityByEntity(entity);
         if(tEntity == null) return;
         tEntity.damage(tPlayer);
+
+        double distance = player.getEyeLocation().distance(entity.getLocation());
+        distance = 3 + tPlayer.getTotalStats().getSwingRange() - distance;
+        distance = player.isSprinting() ? distance * 1.5 : distance;
+        Vector knockback = player.getEyeLocation().getDirection();
+        knockback.normalize();
+        knockback.multiply(distance * 0.24).setY(distance * 0.05);
+        entity.setVelocity(knockback);
+
         entity.damage(0);
     }
 
@@ -35,18 +48,36 @@ public class EntityHandler {
         if(e.getDamage() == 0 || entity == null) return;
         entity.naturalDamage((long) e.getDamage());
     }
+    //init custom Entities here
+    public void initCustomEntities(){
+        customEntities.add(new TestMob());
+    }
 
-    public void register(){
-        for(World world : Togara.instance.getServer().getWorlds()){
-            for(Entity entity : world.getEntities()){
-                if(!(entity instanceof Player) && entity instanceof Mob){
-                    addEntity((LivingEntity) entity);
-                }
+    public void register(World world){
+        if(loadedWorlds.contains(world)) return;
+        loadedWorlds.add(world);
+        for(Entity entity : world.getEntities()){
+            if(entity instanceof ArmorStand && ((ArmorStand) entity).isMarker() || entity instanceof TextDisplay){
+                entity.remove();
+                continue;
+            }
+            String entityId = entity.getPersistentDataContainer().get(new NamespacedKey(Togara.instance, "entityId"), PersistentDataType.STRING);
+            if(entityId != null){
+                TogaraEntity tEntity = getEntityInstance(entityId);
+                if(tEntity == null) continue;
+                tEntity.spawn(entity.getLocation());
+                entity.remove();
+                continue;
+            }
+            if(!(entity instanceof Player) && entity instanceof Mob){
+                addEntity((LivingEntity) entity);
             }
         }
+
     }
     public void unregister(){
         for(TogaraEntity entity : entityList){
+            if(entity instanceof TogaraPlayer) continue;
             entity.getHealthTag().remove();
         }
     }
@@ -71,7 +102,6 @@ public class EntityHandler {
         entityList.removeAll(toRemove);
         toRemove.clear();
 
-
         for(DamageDisplay display : DamageDisplay.damageDisplays) display.update();
         DamageDisplay.clear();
     }
@@ -93,4 +123,12 @@ public class EntityHandler {
         }
         return null;
     }
+
+    public TogaraEntity getEntityInstance(String entityId){
+        for(TogaraEntity entity : customEntities){
+            if(entity.getEntityId().equalsIgnoreCase(entityId)) return entity;
+        }
+        return null;
+    }
+
 }
