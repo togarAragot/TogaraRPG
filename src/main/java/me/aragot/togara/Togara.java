fdfd84e153a4c33f4e8064605c2a21aff57dca8a
togarAragot <the.aragot@gmail.com>
2023-05-17 19:49:51 +0200

    Init dealer and Command

--- src/main/java/me/aragot/togara/Togara.java
+++ src/main/java/me/aragot/togara/Togara.java
@@ -6,6 +6,8 @@ import com.comphenix.protocol.ProtocolManager;
 import com.comphenix.protocol.events.ListenerPriority;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketEvent;
+import me.aragot.togara.admin.items.ItemDealerGui;
+import me.aragot.togara.admin.items.ItemDealerPacketHandler;
 import me.aragot.togara.commands.*;
 import me.aragot.togara.entities.EntityHandler;
 import me.aragot.togara.items.ItemHandler;
@@ -52,17 +54,20 @@ public final class Togara extends JavaPlugin {
         itemHandler = new ItemHandler();
         storageManager = new StorageManager();
         StorageGui.init();
+        ItemDealerGui.init();
 
         this.getCommand("tsummon").setExecutor(new TogaraSummonCommand());
         this.getCommand("test").setExecutor(new TestCommand());
         this.getCommand("tkill").setExecutor(new TogaraKillCommand());
         this.getCommand("storage").setExecutor(new StorageCommand());
         this.getCommand("tgive").setExecutor(new TogaraGiveCommand());
+        this.getCommand("dealer").setExecutor(new DealerCommand());
         manager.registerEvents(new EntitySpawnEvent() , this);
         manager.registerEvents(new TickListener(), this);
         manager.registerEvents(new DamageListener(), this);
         manager.registerEvents(new PlayerListener(), this);
         manager.registerEvents(new StorageGui(), this);
+        manager.registerEvents(new ItemDealerGui(), this);
         manager.registerEvents(new WorldListener(), this);
 
 
@@ -93,6 +98,7 @@ public final class Togara extends JavaPlugin {
             @Override
             public void onPacketReceiving(PacketEvent event) {
                 StoragePacketHandler.onUpdateSignEvent(event);
+                ItemDealerPacketHandler.onUpdateSignEvent(event);
             }
         });
 
