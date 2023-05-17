caf85611a114b7758e95eb7b2abcd41f8553f217
togarAragot <the.aragot@gmail.com>
2023-05-17 19:50:33 +0200

    Added a list to get all itemtype specific items

--- src/main/java/me/aragot/togara/items/TogaraArmor.java
+++ src/main/java/me/aragot/togara/items/TogaraArmor.java
@@ -4,20 +4,24 @@ package me.aragot.togara.items;
 import me.aragot.togara.stats.ItemStats;
 import org.bukkit.Material;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class TogaraArmor extends Stattable {
 
+    public static ArrayList<TogaraItem>  armorItems = new ArrayList<>();
     private static HashMap<String, ItemType> armorList = new HashMap<>();
 
     public TogaraArmor(Material material, String itemId, ItemType type, Rarity rarity, ItemStats stats) {
         super(material, itemId, type, rarity, stats);
         addArmor(itemId, type);
+        armorItems.add(this);
     }
 
     public TogaraArmor(Material material, String displayName, String itemId, ItemType type, Rarity rarity, ItemStats stats) {
         super(material, displayName, itemId, type, rarity, stats);
         addArmor(itemId, type);
+        armorItems.add(this);
     }
 
     public static boolean isItemType(String itemId, ItemType type) {
@@ -27,4 +31,9 @@ public class TogaraArmor extends Stattable {
     public static void addArmor(String itemId, ItemType type){
         armorList.put(itemId, type);
     }
+
+    public static HashMap<String, ItemType> getArmorList(){
+        return armorList;
+    }
+
 }
--- src/main/java/me/aragot/togara/items/TogaraWeapon.java
+++ src/main/java/me/aragot/togara/items/TogaraWeapon.java
@@ -3,20 +3,23 @@ package me.aragot.togara.items;
 import me.aragot.togara.stats.ItemStats;
 import org.bukkit.Material;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class TogaraWeapon extends Stattable{
 
-
+    public static ArrayList<TogaraItem> weaponItems = new ArrayList<>();
     private static final HashMap<String, ItemType> weaponList = new HashMap<>();
 
     public TogaraWeapon(Material material, String itemId, ItemType type, Rarity rarity, ItemStats stats) {
         super(material, itemId, type, rarity, stats);
         addWeapon(itemId, type);
+        weaponItems.add(this);
     }
     public TogaraWeapon(Material material, String displayName, String itemId, ItemType type, Rarity rarity, ItemStats stats) {
         super(material, displayName, itemId, type, rarity, stats);
         addWeapon(itemId, type);
+        weaponItems.add(this);
     }
 
     public static boolean isItemType(String itemId, ItemType type) {
@@ -25,5 +28,9 @@ public class TogaraWeapon extends Stattable{
 
     public static void addWeapon(String itemId, ItemType type){
         weaponList.put(itemId, type);
+    }
+
+    public static HashMap<String, ItemType> getWeapons(){
+        return weaponList;
     }
 }
